6d8a463391a5fcb7d660ae5d7d39c156743f2feb
Brian Starkey <stark3y@gmail.com>
2022-12-05 13:18:19 +0000

    05: Part 2
    Took an annoyingly long time because I once again got caught out by the
    fact that list operations (vs.extend(...)) return "None" in python.

--- 05/puzzle.py
+++ 05/puzzle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import copy
 import sys
 
 stacks = []
@@ -21,6 +22,8 @@ with open(sys.argv[1]) as f:
             i += 4
             stack += 1
 
+    stacks2 = copy.deepcopy(stacks)
+
     for line in f:
         line = line.strip()
         if len(line) == 0:
@@ -28,9 +31,19 @@ with open(sys.argv[1]) as f:
 
         parts = line.split()
         count, src, dst = int(parts[1]), int(parts[3])-1, int(parts[5])-1
+
+        # Part 1
         for i in range(count):        
             v = stacks[src].pop(0)
             stacks[dst].insert(0, v)
 
+        # Part 2
+        vs = copy.copy(stacks2[src][:count])
+        stacks2[src] = stacks2[src][count:]
+        vs.extend(stacks2[dst])
+        stacks2[dst] = vs
+
 vals = [stack[0] for stack in stacks]
 print("Part 1:", ''.join(vals))
+vals = [stack[0] for stack in stacks2]
+print("Part 2:", ''.join(vals))
