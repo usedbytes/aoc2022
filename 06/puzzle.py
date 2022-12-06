b4708a0fd4e5d28b3db14d69ea5f85b4b96e3a4e
Brian Starkey <stark3y@gmail.com>
2022-12-06 15:21:03 +0000

    06: Part 2

--- 06/puzzle.py
+++ 06/puzzle.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 import sys
 
+def find_marker(line, length):
+    i = length - 1
+    while i <= len(line):
+        buf = line[i-(length - 1):i+1]
+        uniques = set(buf)
+        if len(uniques) == length:
+            return i + 1
+        i += 1
+    return -1
+
 with open(sys.argv[1]) as f:
     for line in f:
         line = line.strip('\n')
 
-        i = 3
-        while i <= len(line):
-            buf = line[i-3:i+1]
-            uniques = set(buf)
-            if len(uniques) == 4: 
-                print("Part 1:", i + 1)
-                break
-            i += 1
+        p1 = find_marker(line, 4)
+        print("Part 1:", p1)
+        p2 = find_marker(line, 14)
+        print("Part 2:", p2)
