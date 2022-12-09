d1dd663030bcaba89947d6f739b748faec6d46fa
Brian Starkey <stark3y@gmail.com>
2022-12-09 12:56:34 +0000

    09: Broken Part 2
    I don't know yet why it doesn't work. It works on the samples, but
    apparently gives a result which is too high on my input

--- 09/puzzle.py
+++ 09/puzzle.py
@@ -8,41 +8,54 @@ dirs = {
     "D": ( 0,  1),
 }
 
-hpos = (0, 0)
-tpos = (0, 0)
-positions = {}
+hpos = (0,0)
+knots = [(0,0) for i in range(9)]
+
+p1 = {}
+p2 = {}
 with open(sys.argv[1]) as f:
     for line in f:
         line = line.strip()
         parts = line.split()
         d = dirs[parts[0]]
         c = int(parts[1])
+
         for i in range(c):
             hpos = (hpos[0] + d[0], hpos[1] + d[1])
-            diff = (hpos[0] - tpos[0], hpos[1] - tpos[1])
 
-            dx = 0
-            dy = 0
+            # knot in front
+            kif = hpos
 
-            if diff[0] < -1:
-                dx = -1
-                if diff[1] != 0:
-                    dy = diff[1]
-            elif diff[0] > 1:
-                dx = 1
-                if diff[1] != 0:
-                    dy = diff[1]
+            for i in range(len(knots)):
+                knot = knots[i]
+                diff = (kif[0] - knot[0], kif[1] - knot[1])
 
-            if diff[1] < -1:
-                dy = -1
-                if diff[0] != 0:
-                    dx = diff[0]
-            elif diff[1] > 1:
-                dy = 1
-                if diff[0] != 0:
-                    dx = diff[0]
+                dx = 0
+                dy = 0
 
-            tpos = (tpos[0] + dx, tpos[1] + dy)
-            positions[tpos] = True
+                if diff[0] < -1:
+                    dx = -1
+                    if diff[1] != 0:
+                        dy = diff[1]
+                elif diff[0] > 1:
+                    dx = 1
+                    if diff[1] != 0:
+                        dy = diff[1]
 
-print("Part 1:", len(positions))
+                if diff[1] < -1:
+                    dy = -1
+                    if diff[0] != 0:
+                        dx = diff[0]
+                elif diff[1] > 1:
+                    dy = 1
+                    if diff[0] != 0:
+                        dx = diff[0]
+
+                knots[i] = (knot[0] + dx, knot[1] + dy)
+                kif = knots[i]
+
+            p1[knots[0]] = True
+            p2[knots[-1]] = True
+
+print("Part 1:", len(p1))
+print("Part 2:", len(p2))
