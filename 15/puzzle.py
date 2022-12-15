afed818632ade75e85d1723cd6caf9402be3b236
Brian Starkey <stark3y@gmail.com>
2022-12-15 23:04:14 +0000

    15: Part 2
    Potentially could be faster, as I still go through every single one of
    the 4 million rows. I skip chunks of each row based on how much a sensor
    overlaps with that row. It's not that fast, but manageable.
    
    It takes ~23 seconds on my M1 Air

--- 15/puzzle.py
+++ 15/puzzle.py
@@ -24,8 +24,12 @@ with open(sys.argv[1]) as f:
         sensor_distances[sensor] = distance
 
 y = 10
+xlimit = 20
+ylimit = 20
 if len(sys.argv) > 2:
-    y = int(sys.argv[2])
+    y = 2000000
+    ylimit = 4000000
+    xlimit = 4000000
 
 part1 = {}
 for s, d in sensor_distances.items():
@@ -34,4 +38,24 @@ for s, d in sensor_distances.items():
     for x in range(s[0] - width, s[0] + width + 1):
         if (x, y) not in beacons and (x, y) not in sensor_distances:
             part1[(x, y)] = True
-print(len(part1))
+print("Part 1:", len(part1))
+
+def search(xlimit, ylimit):
+    y = 0
+    while y < ylimit:
+        x = 0
+        while x < xlimit:
+            pos = (x, y)
+            for s, d in sensor_distances.items():
+                vdist = abs(y - s[1])
+                width = d - vdist
+                if s[0] - width - 1 < x <= s[0] + width:
+                    x = s[0] + width + 1
+                    continue
+            if (x, y) == pos:
+                return pos
+        y += 1
+    return None
+
+pos = search(xlimit, ylimit)
+print("Part 2:", pos[0] * 4000000 + pos[1])
